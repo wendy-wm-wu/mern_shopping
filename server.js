72524444b78a304fd81d5b5c2789f8c9dd2ae12d
Wendy Wu <wendywu@gitlab.com>
2021-03-13 14:59:08 -0800

    Set up heroku for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -20,7 +21,18 @@ mongoose
   })
   .catch(err => console.log(err));
 
+  // Use routes 
   app.use('/api/items', items);
+
+  // Serve static assets if in production
+  if (process.env.NODE.ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
   const port = process.env.PORT || 5000;
 
