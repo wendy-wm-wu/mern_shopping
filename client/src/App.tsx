a2bb12c5ff7ae702b481482ca509e210bdeeecf5
Wendy Wu <wendywu@gitlab.com>
2021-03-13 10:47:04 -0800

    Add redux libraries, create store, and connect app to store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './flux/store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 };
 
