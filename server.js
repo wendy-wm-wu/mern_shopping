d9d648fe09ac9efffdb231018f03644bfeee8ba7
Wendy Wu <wendywu@gitlab.com>
2021-03-13 17:42:53 -0800

    version 1.0.0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require ('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, './.env')});
 
 const items = require('./routes/api/items');
 
